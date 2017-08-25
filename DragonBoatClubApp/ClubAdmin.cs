310d645f7045bd6dcefa3ed0a58e26a79cc78452
Elaine Hall <elrozhall@hotmail.com>
2017-08-25 16:54:21 -0700

    hopefully this third time is the charm for my first real commit

---
+++ DragonBoatClubApp/ClubAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonBoatClubApp
+{
+	public static class ClubAdmin
+	{
+		public static DBClub AddClub()
+	}
+}
---
+++ DragonBoatClubApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonBoatClubApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var myDBClub = new DBClub
+			{
+				ClubName = "Seattle Flying Dragon Boat Club",
+				ClubWebsite = "www.seattleflyingdragons.org",
+				//myDBClub.DateAdded = DateTime.Now;
+				SkillLevel = SkillLevels.Advanced,
+				Cost = CostTypes.FreeFirst3Times
+			};
+
+		//myDBClub.Deposit(500.01M);
+		//myDBClub.Balance = 123456.54M;
+
+			Console.WriteLine($"myDBClub: {myDBClub.ClubId}, DBClub Name: {myDBClub.ClubName}, Balance: {myDBClub.Balance:C}, Date Club Added: {myDBClub.DateAdded}");
+
+			var myDBClub2 = new DBClub
+			{
+				ClubName = "Club 2 Placeholder",
+				ClubWebsite = "www.seattleflyingdragons.org"
+
+			};
+
+		//myDBClub2.DateAdded = DateTime.Now;
+
+		Console.WriteLine($"myDBClub: {myDBClub2.ClubId}, DBClub Name: {myDBClub2.ClubName}, Balance: {myDBClub2.Balance:C}, Date Club Added: {myDBClub2.DateAdded}");
+
+		}
+
+		
+	}
+}
